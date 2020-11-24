03f92c14f60b4a8727fffda41b7c310761ca8f18
MMichotte <martin.michotte@gmail.com>
2020-11-24 13:29:41 +0100

    basic nodeJs backend with car api

--- backend/routes/car.js
+++ backend/routes/car.js
@@ -3,8 +3,6 @@ import {carController} from '../controllers'
 import makeCallback from '../helpers/express-callback'
 
 const router = express.Router();
-//const Thing = require('../models/thing');
-//const stuffController = require('../controllers/stuff')
 
 router.get('/cars', makeCallback(carController.getAllCars));
 
