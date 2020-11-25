f9e459f8205680db2934bf5eca9ca113b3fb1ba6
MMichotte <martin.michotte@gmail.com>
2020-11-25 20:20:01 +0100

    updated packages

--- frontend/src/app/cars/cars.component.spec.ts
+++ frontend/src/app/cars/cars.component.spec.ts
@@ -1,5 +1,5 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
 
 import { CarsComponent } from './cars.component';
 
