dc2ab672134b82a185b58685bd853aa206240dac
MMichotte <martin.michotte@gmail.com>
2020-11-27 13:48:07 +0100

    updated test with refactoring

--- backend/tests/car.test.js
+++ backend/tests/car.test.js
@@ -3,7 +3,7 @@ import {expect} from 'chai'
 import chaiHttp from 'chai-http'
 
 import server from '../server';
-import carController from '../controllers/carController'
+import * as carController from '../controllers/carController'
 import carsMock from './mocks/car'
 
 
